e8eb51c85334aacb95dcd1418bc5955939ef2d22
julia <jumangelozi@gmail.com>
2025-05-06 17:47:07 -0300

    Adicionados DTO, Service e Controller de contato

--- challenge-api/challenge-api/src/main/java/gp/moto/challenge_api/model/Contato.java
+++ challenge-api/challenge-api/src/main/java/gp/moto/challenge_api/model/Contato.java
@@ -1,16 +1,21 @@
 package gp.moto.challenge_api.model;
 
 
+import gp.moto.challenge_api.dto.ContatoDTO;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.beans.BeanUtils;
 
 @Data
 @Entity
 @Table(name = "t_gpMottu_contato")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Contato {
 
     @Id
@@ -22,7 +27,7 @@ public class Contato {
     private String nmDono;
 
     @NotNull(message = "Valor inválido para status")
-    @Length(max = 1, message = "Valor inválido para status. O valor deve ter no máximo 1 caracteres")
+    @Max(value = 1, message = "Valor inválido para status. O valor deve ter no máximo 1 caracteres")
     private int status;
 
     @NotEmpty(message = "Valor inválido para email")
@@ -33,4 +38,8 @@ public class Contato {
     @OneToOne
     @JoinColumn(name = "id_telefone")
     private Telefone idTelefone;
+
+    public Contato(ContatoDTO contatoDTO) {
+        BeanUtils.copyProperties(contatoDTO, this);
+    }
 }
