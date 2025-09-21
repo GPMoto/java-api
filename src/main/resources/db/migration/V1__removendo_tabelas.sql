c5c94299f2edbb2518a6a3347e844935c28a62a2
feliperibeiroa <feliperibeirots@gmail.com>
2025-09-21 16:59:15 -0300

    feita a migração para o banco sql server

--- src/main/resources/db/migration/V1__removendo_tabelas.sql
+++ src/main/resources/db/migration/V1__removendo_tabelas.sql
@@ -1,28 +1,84 @@
 
-drop table if exists t_gp_mottu_cidade cascade;
 
-drop table if exists t_gp_mottu_contato cascade;
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'unique_id_telefone')
+ALTER TABLE t_gp_mottu_cidade DROP CONSTRAINT unique_id_telefone;
 
-drop table if exists t_gp_mottu_endereco cascade;
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'unique_cep')
+ALTER TABLE t_gp_mottu_contato DROP CONSTRAINT unique_cep;
 
-drop table if exists t_gp_mottu_estado cascade;
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'idx_filial_contato')
+ALTER TABLE t_gp_mottu_endereco DROP CONSTRAINT idx_filial_contato;
 
-drop table if exists t_gp_mottu_filial cascade;
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'idx_filial_endereco')
+ALTER TABLE t_gp_mottu_estado DROP CONSTRAINT idx_filial_endereco;
 
-drop table if exists t_gp_mottu_moto cascade;
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'unique_cnpj')
+ALTER TABLE t_gp_mottu_filial DROP CONSTRAINT unique_cnpj;
 
-drop table if exists t_gp_mottu_pais cascade;
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'unique_identificador')
+ALTER TABLE t_gp_mottu_filial DROP CONSTRAINT unique_identificador;
 
-drop table if exists t_gp_mottu_perfil cascade;
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'idx_qrcode_moto')
+ALTER TABLE t_gp_mottu_moto DROP CONSTRAINT idx_qrcode_moto;
 
-drop table if exists t_gp_mottu_qrcode cascade;
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'unique_email')
+ALTER TABLE t_gp_mottu_moto DROP CONSTRAINT unique_email;
 
-drop table if exists t_gp_mottu_secoes_filial cascade;
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'fk_cidade_estado')
+ALTER TABLE t_gp_mottu_qrcode DROP CONSTRAINT fk_cidade_estado;
 
-drop table if exists t_gp_mottu_telefone cascade;
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'fk_contato_telefone')
+ALTER TABLE t_gp_mottu_qrcode DROP CONSTRAINT fk_contato_telefone;
 
-drop table if exists t_gp_mottu_tipo_moto cascade;
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'fk_endereco_cidade')
+ALTER TABLE t_gp_mottu_secoes_filial DROP CONSTRAINT fk_endereco_cidade;
 
-drop table if exists t_gp_mottu_tipo_secao cascade;
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'fk_estado_pais')
+ALTER TABLE t_gp_mottu_secoes_filial DROP CONSTRAINT fk_estado_pais;
 
-drop table if exists t_gp_mottu_usuario cascade;
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'fk_filial_contato')
+ALTER TABLE t_gp_mottu_usuario DROP CONSTRAINT fk_filial_contato;
+
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'fk_filial_endereco')
+ALTER TABLE t_gp_mottu_usuario DROP CONSTRAINT fk_filial_endereco;
+
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'fk_moto_filial')
+ALTER TABLE t_gp_mottu_usuario DROP CONSTRAINT fk_moto_filial;
+
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'fk_moto_tipo_moto')
+ALTER TABLE t_gp_mottu_usuario DROP CONSTRAINT fk_moto_tipo_moto;
+
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'fk_qrcode_filial')
+ALTER TABLE t_gp_mottu_usuario DROP CONSTRAINT fk_qrcode_filial;
+
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'fk_qrcode_moto')
+ALTER TABLE t_gp_mottu_usuario DROP CONSTRAINT fk_qrcode_moto;
+
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'fk_secoes_filial_filial')
+ALTER TABLE t_gp_mottu_usuario DROP CONSTRAINT fk_secoes_filial_filial;
+
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'fk_secoes_filial_tipo_secao')
+ALTER TABLE t_gp_mottu_usuario DROP CONSTRAINT fk_secoes_filial_tipo_secao;
+
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'fk_usuario_filial')
+ALTER TABLE t_gp_mottu_usuario DROP CONSTRAINT fk_usuario_filial;
+
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE name = 'fk_usuario_perfil')
+ALTER TABLE t_gp_mottu_usuario DROP CONSTRAINT fk_usuario_perfil;
+
+
+-- Drop tables
+IF OBJECT_ID('t_gp_mottu_cidade', 'U') IS NOT NULL DROP TABLE t_gp_mottu_cidade;
+IF OBJECT_ID('t_gp_mottu_contato', 'U') IS NOT NULL DROP TABLE t_gp_mottu_contato;
+IF OBJECT_ID('t_gp_mottu_endereco', 'U') IS NOT NULL DROP TABLE t_gp_mottu_endereco;
+IF OBJECT_ID('t_gp_mottu_estado', 'U') IS NOT NULL DROP TABLE t_gp_mottu_estado;
+IF OBJECT_ID('t_gp_mottu_filial', 'U') IS NOT NULL DROP TABLE t_gp_mottu_filial;
+IF OBJECT_ID('t_gp_mottu_moto', 'U') IS NOT NULL DROP TABLE t_gp_mottu_moto;
+IF OBJECT_ID('t_gp_mottu_pais', 'U') IS NOT NULL DROP TABLE t_gp_mottu_pais;
+IF OBJECT_ID('t_gp_mottu_perfil', 'U') IS NOT NULL DROP TABLE t_gp_mottu_perfil;
+IF OBJECT_ID('t_gp_mottu_qrcode', 'U') IS NOT NULL DROP TABLE t_gp_mottu_qrcode;
+IF OBJECT_ID('t_gp_mottu_secoes_filial', 'U') IS NOT NULL DROP TABLE t_gp_mottu_secoes_filial;
+IF OBJECT_ID('t_gp_mottu_telefone', 'U') IS NOT NULL DROP TABLE t_gp_mottu_telefone;
+IF OBJECT_ID('t_gp_mottu_tipo_moto', 'U') IS NOT NULL DROP TABLE t_gp_mottu_tipo_moto;
+IF OBJECT_ID('t_gp_mottu_tipo_secao', 'U') IS NOT NULL DROP TABLE t_gp_mottu_tipo_secao;
+IF OBJECT_ID('t_gp_mottu_usuario', 'U') IS NOT NULL DROP TABLE t_gp_mottu_usuario;
--- src/main/resources/db/migration/V5__alter_tables.sql
+++ src/main/resources/db/migration/V5__alter_tables.sql
@@ -1,70 +1,94 @@
 
-alter table if exists t_gp_mottu_cidade
-    add constraint FKn0c2rm0wlm1x83uo8k91pfeiy
-    foreign key (id_estado)
-    references t_gp_mottu_estado;
+alter table t_gp_mottu_contato
+    add constraint unique_id_telefone unique (id_telefone);
 
-alter table if exists t_gp_mottu_contato
-    add constraint FK6ln1g62h95fr1mer3182iyu7s
-    foreign key (id_telefone)
-    references t_gp_mottu_telefone;
+alter table t_gp_mottu_endereco
+    add constraint unique_cep unique (cep);
 
-alter table if exists t_gp_mottu_endereco
-    add constraint FKd0k457tjrbsgfjbv0rguqvvlh
-    foreign key (id_cidade)
-    references t_gp_mottu_cidade;
+create unique nonclustered index idx_filial_contato
+       on t_gp_mottu_filial (id_contato) where id_contato is not null;
 
-alter table if exists t_gp_mottu_estado
-    add constraint FK318583hta83ah6tw6jwbi29xl
-    foreign key (id_pais)
-    references t_gp_mottu_pais;
+    create unique nonclustered index idx_filial_endereco
+       on t_gp_mottu_filial (id_endereco) where id_endereco is not null;
 
-alter table if exists t_gp_mottu_filial
-    add constraint FKirj4dfxk02k7y73eobui58gk6
-    foreign key (id_contato)
-    references t_gp_mottu_contato;
+alter table t_gp_mottu_filial
+    add constraint unique_cnpj unique (cnpj_filial);
 
-alter table if exists t_gp_mottu_filial
-    add constraint FK6ggv5pafxnkja30914uy9msuk
-    foreign key (id_endereco)
-    references t_gp_mottu_endereco;
+alter table t_gp_mottu_moto
+    add constraint unique_identificador unique (identificador);
 
-alter table if exists t_gp_mottu_moto
-    add constraint FKfj91oxndb5ja9r8r3h9rsq3uj
-    foreign key (id_secao)
-    references t_gp_mottu_secoes_filial;
+create unique nonclustered index idx_qrcode_moto
+       on t_gp_mottu_qrcode (id_moto) where id_moto is not null;
 
-alter table if exists t_gp_mottu_moto
-    add constraint FKgetdjg42kkbfmo0fiygbenbo7
-    foreign key (id_tipo_moto)
-    references t_gp_mottu_tipo_moto;
+alter table t_gp_mottu_usuario
+    add constraint unique_email unique (nm_email);
 
-alter table if exists t_gp_mottu_qrcode
-    add constraint FKg7ih1f1hpbh0842gn9byeou2l
-    foreign key (id_filial)
-    references t_gp_mottu_filial;
+alter table t_gp_mottu_cidade
+    add constraint fk_cidade_estado
+        foreign key (id_estado)
+            references t_gp_mottu_estado;
 
-alter table if exists t_gp_mottu_qrcode
-    add constraint FKfnfd3weesmrb893ro7ws4v8l5
-    foreign key (id_moto)
-    references t_gp_mottu_moto;
+alter table t_gp_mottu_contato
+    add constraint fk_contato_telefone
+        foreign key (id_telefone)
+            references t_gp_mottu_telefone;
 
-alter table if exists t_gp_mottu_secoes_filial
-    add constraint FKbaac61mb4bwwxnsefictwv1qa
-    foreign key (id_filial)
-    references t_gp_mottu_filial;
+alter table t_gp_mottu_endereco
+    add constraint fk_endereco_cidade
+        foreign key (id_cidade)
+            references t_gp_mottu_cidade;
 
-alter table if exists t_gp_mottu_secoes_filial
-    add constraint FKm97wo6c4hg9nffy25yg3kaaid
-    foreign key (id_tipo_secao)
-    references t_gp_mottu_tipo_secao;
+alter table t_gp_mottu_estado
+    add constraint fk_estado_pais
+        foreign key (id_pais)
+            references t_gp_mottu_pais;
 
-alter table if exists t_gp_mottu_usuario
-    add constraint FKp6b9tjnjgbipjatqey31wxjs6
-    foreign key (id_filial)
-    references t_gp_mottu_filial;
+alter table t_gp_mottu_filial
+    add constraint fk_filial_contato
+        foreign key (id_contato)
+            references t_gp_mottu_contato;
 
-alter table if exists t_gp_mottu_usuario
-    add constraint FK5tksjicfcljksrneuvisppeev
-    foreign key (id_perfil)
-    references t_gp_mottu_perfil;
+alter table t_gp_mottu_filial
+    add constraint fk_filial_endereco
+        foreign key (id_endereco)
+            references t_gp_mottu_endereco;
+
+alter table t_gp_mottu_moto
+    add constraint fk_moto_filial
+        foreign key (id_secao)
+            references t_gp_mottu_secoes_filial;
+
+alter table t_gp_mottu_moto
+    add constraint fk_moto_tipo_moto
+        foreign key (id_tipo_moto)
+            references t_gp_mottu_tipo_moto;
+
+alter table t_gp_mottu_qrcode
+    add constraint fk_qrcode_filial
+        foreign key (id_filial)
+            references t_gp_mottu_filial;
+
+alter table t_gp_mottu_qrcode
+    add constraint fk_qrcode_moto
+        foreign key (id_moto)
+            references t_gp_mottu_moto;
+
+alter table t_gp_mottu_secoes_filial
+    add constraint fk_secoes_filial_filial
+        foreign key (id_filial)
+            references t_gp_mottu_filial;
+
+alter table t_gp_mottu_secoes_filial
+    add constraint fk_secoes_filial_tipo_secao
+        foreign key (id_tipo_secao)
+            references t_gp_mottu_tipo_secao;
+
+alter table t_gp_mottu_usuario
+    add constraint fk_usuario_filial
+        foreign key (id_filial)
+            references t_gp_mottu_filial;
+
+alter table t_gp_mottu_usuario
+    add constraint fk_usuario_perfil
+        foreign key (id_perfil)
+            references t_gp_mottu_perfil;
